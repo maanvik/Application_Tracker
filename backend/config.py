acbbcabfd07c58f5b2dca328258d622fa2b0ac1d
Maanvik <thodupunurimaanvik25@gmail.com>
2025-02-04 01:02:47 -0500

    remove files added in gitignore

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = "thatsTrueToBeReal"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Mandy%401234@localhost/job_tracker'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
